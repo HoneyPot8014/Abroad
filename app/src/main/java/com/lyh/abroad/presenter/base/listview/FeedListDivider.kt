a31529ab9403f3f7fac0876fa9de58ab27c94923
이용현 <gch02310@naver.com>
2020-01-05 16:24:09 +0900

    Feed db 구조 변화로 인한, entity 필드 추가
    - RecyclerView 베이스 divider 클래스 추가

---
+++ app/src/main/java/com/lyh/abroad/presenter/base/listview/FeedListDivider.kt
@@ -0,0 +1,39 @@
+package com.lyh.abroad.presenter.base.listview
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import androidx.core.view.children
+import androidx.recyclerview.widget.RecyclerView
+import com.lyh.abroad.presenter.util.DisplayUtil
+
+class FeedListDivider(
+    private val dp: Float,
+    private val color: Int = 0xFFebebeb.toInt()
+) : RecyclerView.ItemDecoration() {
+
+    private val canvas = Canvas()
+    private val paint = Paint()
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        parent.children.forEachIndexed { index, view ->
+            if (index != parent.childCount) {
+                val top =
+                    view.bottom + (view.layoutParams as RecyclerView.LayoutParams).bottomMargin
+                val bottom = top + DisplayUtil.dpToPx(parent.context, dp) + top
+
+                canvas.drawRect(
+                    left.toFloat(),
+                    top.toFloat(),
+                    right.toFloat(),
+                    bottom.toFloat(),
+                    paint.apply {
+                        this.color = this@FeedListDivider.color
+                    }
+                )
+            }
+        }
+    }
+}
