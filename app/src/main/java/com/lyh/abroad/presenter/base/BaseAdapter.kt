b2a95d4c48f3b79084774af7120028400b6ad44c
이용현 <gch02310@naver.com>
2020-01-05 15:16:15 +0900

    RecyclerView base 추가

---
+++ app/src/main/java/com/lyh/abroad/presenter/base/BaseAdapter.kt
@@ -0,0 +1,24 @@
+package com.lyh.abroad.presenter.base
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+abstract class BaseAdapter<T, VH : BaseViewHolder<T>>(
+    diffCallback: DiffUtil.ItemCallback<T> = object : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+            return oldItem?.equals(newItem) ?: false
+        }
+
+        override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+) : ListAdapter<T, VH>(diffCallback) {
+
+    abstract override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
