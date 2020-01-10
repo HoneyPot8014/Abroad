28ae1333e3cbae481b72f734db63475f8be616f4
이용현 <gch02310@naver.com>
2020-01-11 02:34:47 +0900

    국가 autoComplete 로직 추가
    - 초성검색 가능
    - query가 2글자 이상일 경우 처리 확인 필요

---
+++ app/src/main/java/com/lyh/abroad/domain/interactor/place/GetCountryUsecase.kt
@@ -0,0 +1,113 @@
+package com.lyh.abroad.domain.interactor.place
+
+
+import com.lyh.abroad.domain.entity.CountryEntity
+import com.lyh.abroad.domain.interactor.BaseUsecase
+import com.lyh.abroad.domain.model.ResultModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.*
+
+class GetCountryUsecase(
+
+) : BaseUsecase<List<CountryEntity>, GetCountryUsecase.CountryParam>() {
+
+    data class CountryParam(val query: String?) : Param()
+
+    private val singleChosungMap = mapOf(
+        'ㄱ' to 0x1100,
+        'ㄲ' to 0x1101,
+        'ㄴ' to 0x1102,
+        'ㄷ' to 0x1103,
+        'ㄸ' to 0x1104,
+        'ㄹ' to 0x1105,
+        'ㅁ' to 0x1106,
+        'ㅂ' to 0x1107,
+        'ㅃ' to 0x1108,
+        'ㅅ' to 0x1109,
+        'ㅆ' to 0x110A,
+        'ㅇ' to 0x110B,
+        'ㅈ' to 0x110C,
+        'ㅉ' to 0x110D,
+        'ㅊ' to 0x110E,
+        'ㅋ' to 0x110F,
+        'ㅌ' to 0x1110,
+        'ㅍ' to 0x1111,
+        'ㅎ' to 0x1112
+    )
+
+    private val test = Locale.getISOCountries().map {
+        Locale("ko", it)
+    }.sortedBy {
+        it.displayName
+    }.map {
+        CountryEntity(
+            it.country.toFlagEmoji(),
+            it.country,
+            it.displayCountry,
+            null
+        )
+    }
+
+    override suspend fun bindUsecase(param: CountryParam?): ResultModel<List<CountryEntity>> {
+        return param?.query?.let { query ->
+            withContext(Dispatchers.Default) {
+                test.filter {
+                    if (it.countryName.any { char -> char in '가'..'힣' }) {
+                        var lastIndex = 0
+                        var count = 0
+                        val set = mutableSetOf<Int>()
+                        for (queryIndex in query.indices) {
+                            for (countryIndex in it.countryName.indices) {
+                                val queryInt = singleChosungMap[query[queryIndex]] ?: query[queryIndex].toInt()
+                                val countryFirstInt = it.countryName[countryIndex].toInt().toFirstUniCode()
+                                if (queryInt == countryFirstInt) {
+                                    if (lastIndex <= countryIndex && !set.contains(queryInt)) {
+                                        lastIndex = countryIndex
+                                        set.add(queryInt)
+                                        count++
+                                    }
+                                } else {
+                                    val countrySecondInt = it.countryName[countryIndex].toInt().toSecondUniCode()
+                                    val querySecondInt = queryInt.toSecondUniCode()
+                                    if (countrySecondInt == querySecondInt) {
+                                        if (queryInt % 28 != 0) {
+                                            if (queryInt == it.countryName[countryIndex].toInt() && lastIndex <= countryIndex && !set.contains(queryInt)) {
+                                                lastIndex = countryIndex
+                                                set.add(queryInt)
+                                                count++
+                                            }
+                                        } else if (lastIndex <= countryIndex && !set.contains(queryInt)) {
+                                            lastIndex = countryIndex
+                                            set.add(queryInt)
+                                            count++
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        return@filter count == query.length
+                    }
+                    return@filter false
+                }.let { ResultModel.onSuccess(it) }
+            }
+        } ?: ResultModel.onSuccess(test)
+    }
+
+    private fun String.toFlagEmoji(): String {
+        if (this.length != 2) {
+            return this
+        }
+        val countryCodeCaps = this.toUpperCase()
+        val firstLetter = Character.codePointAt(countryCodeCaps, 0) - 0x41 + 0x1F1E6
+        val secondLetter = Character.codePointAt(countryCodeCaps, 1) - 0x41 + 0x1F1E6
+        if (!countryCodeCaps[0].isLetter() || !countryCodeCaps[1].isLetter()) {
+            return this
+        }
+
+        return String(Character.toChars(firstLetter)) + String(Character.toChars(secondLetter))
+    }
+
+    private fun Int.toFirstUniCode(): Int = (this - 44032) / 588 + 0x1100
+    private fun Int.toSecondUniCode(): Int = (this - 44032) / 28 + 0x1161
+}
